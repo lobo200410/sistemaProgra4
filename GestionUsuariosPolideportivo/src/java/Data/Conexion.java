4e42ac172b530cb4f7ce1b6c55ff527cb2b4c1f2
Ricardo <148652964+ricr78@users.noreply.github.com>
2025-09-21 21:28:53 -0600

    Conexion
    clase conexion y vista login

---
+++ GestionUsuariosPolideportivo/src/java/Data/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Data;
+
+/**
+ *
+ * @author ricar
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexion {
+    private static final String URL ="jdbc:mysql://localhost:3306/GestionUsuarios";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    
+    public static Connection getConexion() throws SQLException{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection(URL,USER,PASSWORD);
+        }catch(ClassNotFoundException e){
+            throw new SQLException("Error a cargar el drive de la base de datos");
+        }
+    }
+}
---
+++ GestionUsuariosPolideportivo/web/login.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : login
+    Created on : 21 sept 2025, 20:57:44
+    Author     : ricar
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Inicio de Sesión</title>
+        
+    </head>
+    <body>
+        
+    </body>
+</html>
