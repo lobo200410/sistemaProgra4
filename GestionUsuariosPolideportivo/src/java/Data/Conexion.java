a4285ec102dc87287677d47ebedbc9d7cb73b0dc
Ricardo <148652964+ricr78@users.noreply.github.com>
2025-09-22 16:13:35 -0600

    Update Conexion.java
    Actualizacion de la base de datos

--- GestionUsuariosPolideportivo/src/java/Data/Conexion.java
+++ GestionUsuariosPolideportivo/src/java/Data/Conexion.java
@@ -12,7 +12,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class Conexion {
-    private static final String URL ="jdbc:mysql://localhost:3306/GestionUsuarios";
+    private static final String URL ="jdbc:mysql://localhost:3306/gestionusuariospolideportivo";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
     
@@ -25,3 +25,4 @@ public class Conexion {
         }
     }
 }
+
